256c1818cefcf0fb4963d39909a2a5dd59a2c51c
James De La Torre <jamesdelatorre@mail.com>
2018-05-29 15:06:38 -0400

    new commit example_loop

---
+++ Day1Bash/example_loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+greeting='Hello World' 
+#creating variable greeting
+
+echo $greeting   #printing contents of greeting
+
+#Touch and pring range of files in a loop
+
+for myfile in {A..Z}.txt;
+do 
+
+    touch $myfile
+    echo $myfile
+done
